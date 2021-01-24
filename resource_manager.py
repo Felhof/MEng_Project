f45111d01ef3414565f4913db96c60478fcb93b1
Felhof <Felhof1@hotmail.com>
2021-01-24 17:14:48 +0000

    complete marl resource manager

--- resource_manager.py
+++ resource_manager.py
@@ -12,6 +12,7 @@ from resource_allocation_problem import ResourceAllocationProblem
 from rap_environment import ResourceAllocationEnvironment, MDPResourceAllocationEnvironment, RestrictedMDPResourceAllocationEnvironment
 from MDP import MDPBuilder, RestrictedMDP
 import MTA
+from multistage_model import MultiStageActorCritic
 
 
 class BaseResourceManager:
@@ -28,6 +29,68 @@ class BaseResourceManager:
 
         self.ra_problem = ResourceAllocationProblem(rewards, resource_requirements, max_resource_availabilities,
                                                     task_arrival_p, task_departure_p)
+
+    def plot_training_results(self, xlabel="episode", ylabel="cumulative reward", filename="reward"):
+        x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+        plt.figure(figsize=(20, 10))
+        plt.plot(x, y, "b", label="RL Agent")
+        plt.legend()
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.show()
+        plt.savefig(filename)
+
+    def evaluate_model(self, n_episodes=10, episode_length=500, render=False):
+        print("Comparing Model to optimal strategy...")
+        model_rewards = [
+            self.get_model_solution(episode_length=episode_length, render=render) for _ in range(n_episodes)
+        ]
+        optimal_strategy_rewards = [
+            self.calculate_optimal_solution(episode_length=episode_length) for _ in range(n_episodes)
+        ]
+        print("In {0} episodes the model achieved an average reward of: {1}".format(
+            n_episodes,
+            np.mean(model_rewards)
+        ))
+        print("The optimal strategy achieved an average reward of: {1}".format(
+            n_episodes,
+            np.mean(optimal_strategy_rewards)
+        ))
+
+    def get_model_solution(self, episode_length=500, render=False):
+        reward = 0
+        observation = self.vector_environment.reset()
+        for _ in range(episode_length):
+            action, _ = self.model.predict(observation, deterministic=True)
+            observation, r, _, _ = self.vector_environment.step(action)
+            reward += r
+            if render:
+                self.vector_environment.render(mode='console')
+
+        return reward
+
+    def calculate_optimal_solution(self, episode_length=500):
+        self.ra_problem.reset()
+        observation = self.vector_environment.reset()
+        reward = 0
+        for _ in range(episode_length):
+            action = self.ra_problem.get_heuristic_solution(observation)
+            observation, r, _, _ = self.vector_environment.step(action)
+            reward += r
+
+        return reward
+
+    def print_policy(self):
+        all_observations = self.environment.enumerate_observations()
+        policy = {}
+
+        for observation in all_observations:
+            action = self.model.predict(observation, deterministic=True)
+            policy[tuple(observation)] = action
+
+        for item in policy.items():
+            print("{0} : {1}".format(item[0], list(item[1])))
+
 
 
 class ResourceManager(BaseResourceManager):
@@ -55,67 +118,6 @@ class ResourceManager(BaseResourceManager):
             # This is equivalent to callback=CallbackList([progress_callback, auto_save_callback])
             self.model.learn(total_timesteps=self.training_steps, callback=[progress_callback, auto_save_callback])
 
-    def plot_training_results(self, xlabel="episode", ylabel="cumulative reward", filename="reward"):
-        x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-        plt.figure(figsize=(20, 10))
-        plt.plot(x, y, "b", label="RL Agent")
-        plt.legend()
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.show()
-        plt.savefig(filename)
-
-    def evaluate_model(self, n_episodes=10, episode_length=500, render=False):
-        print("Comparing Model to optimal strategy...")
-        model_rewards = [
-            self.get_model_solution(episode_length=episode_length, render=render) for _ in range(n_episodes)
-        ]
-        optimal_strategy_rewards = [
-            self.calculate_optimal_solution(episode_length=episode_length) for _ in range(n_episodes)
-        ]
-        print("In {0} episodes the model achieved an average reward of: {1}".format(
-            n_episodes,
-            np.mean(model_rewards)
-        ))
-        print("The optimal strategy achieved an average reward of: {1}".format(
-            n_episodes,
-            np.mean(optimal_strategy_rewards)
-        ))
-
-    def get_model_solution(self, episode_length=500, render=False):
-        reward = 0
-        observation = self.vector_environment.reset()
-        for _ in range(episode_length):
-            action, _ = self.model.predict(observation, deterministic=True)
-            observation, r, _, _ = self.vector_environment.step(action)
-            reward += r
-            if render:
-                self.vector_environment.render(mode='console')
-
-        return reward
-
-    def calculate_optimal_solution(self, episode_length=500):
-        self.ra_problem.reset()
-        observation = self.vector_environment.reset()
-        reward = 0
-        for _ in range(episode_length):
-            action = self.ra_problem.get_heuristic_solution(observation)
-            observation, r, _, _ = self.vector_environment.step(action)
-            reward += r
-
-        return reward
-
-    def print_policy(self):
-        all_observations = self.environment.enumerate_observations()
-        policy = {}
-
-        for observation in all_observations:
-            action = self.model.predict(observation, deterministic=True)
-            policy[tuple(observation)] = action
-
-        for item in policy.items():
-            print("{0} : {1}".format(item[0], list(item[1])))
-
 
 class MultiAgentResourceManager(BaseResourceManager):
 
@@ -142,9 +144,9 @@ class MultiAgentResourceManager(BaseResourceManager):
 
         self.model = None
         self.training_steps = training_steps
+        self.steps_per_episode = steps_per_episode
 
     def train_model(self):
-
         state_idx_to_model = {}
         models = []
 
@@ -168,63 +170,9 @@ class MultiAgentResourceManager(BaseResourceManager):
                 values.update({idx: value for idx, value in zip(state_idxs, model.value[state_idxs])})
             lower_level_values = values
 
-    def plot_training_results(self, xlabel="episode", ylabel="cumulative reward", filename="reward"):
-        x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-        plt.figure(figsize=(20, 10))
-        plt.plot(x, y, "b", label="RL Agent")
-        plt.legend()
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.show()
-        plt.savefig(filename)
-
-    def evaluate_model(self, n_episodes=10, episode_length=500, render=False):
-        print("Comparing Model to optimal strategy...")
-        model_rewards = [
-            self.get_model_solution(episode_length=episode_length, render=render) for _ in range(n_episodes)
-        ]
-        optimal_strategy_rewards = [
-            self.calculate_optimal_solution(episode_length=episode_length) for _ in range(n_episodes)
-        ]
-        print("In {0} episodes the model achieved an average reward of: {1}".format(
-            n_episodes,
-            np.mean(model_rewards)
-        ))
-        print("The optimal strategy achieved an average reward of: {1}".format(
-            n_episodes,
-            np.mean(optimal_strategy_rewards)
-        ))
-
-    def get_model_solution(self, episode_length=500, render=False):
-        reward = 0
-        observation = self.vector_environment.reset()
-        for _ in range(episode_length):
-            action, _ = self.model.predict(observation, deterministic=True)
-            observation, r, _, _ = self.vector_environment.step(action)
-            reward += r
-            if render:
-                self.vector_environment.render(mode='console')
-
-        return reward
-
-    def calculate_optimal_solution(self, episode_length=500):
-        self.ra_problem.reset()
-        observation = self.vector_environment.reset()
-        reward = 0
-        for _ in range(episode_length):
-            action = self.ra_problem.get_heuristic_solution(observation)
-            observation, r, _, _ = self.vector_environment.step(action)
-            reward += r
-
-        return reward
-
-    def print_policy(self):
-        all_observations = self.environment.enumerate_observations()
-        policy = {}
-
-        for observation in all_observations:
-            action = self.model.predict(observation, deterministic=True)
-            policy[tuple(observation)] = action
-
-        for item in policy.items():
-            print("{0} : {1}".format(item[0], list(item[1])))
+        whole_environment = MDPResourceAllocationEnvironment(self.ra_problem, self.steps_per_episode)
+        whole_vector_environment = make_vec_env(lambda: whole_environment, n_envs=1, monitor_dir=self.log_dir)
+        multistage_model = A2C(MultiStageActorCritic, whole_vector_environment, verbose=1, tensorboard_log=self.log_dir,
+                               stage1_models=models)
+        with ProgressBarManager(self.training_steps) as progress_callback:
+            multistage_model.learn(total_timesteps=self.training_steps, callback=progress_callback)
